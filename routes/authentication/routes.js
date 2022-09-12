420077bba9b32f9891068da76d7f393434c9f488
premrajzambre <premrajzambre@gmail.com>
2022-09-13 00:41:38 +0530

    routes added.

--- routes/authentication/routes.js
+++ routes/authentication/routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-//Route for Video
-// router.use("/auth", require("./registration/routes"));
+//Route for registration
+router.use("/registration", require("./registration/routes"));
+
+//Route for login
+router.use("/login", require("./login/routes"));
 
 module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-//Route for Video
-router.use("/zerozilla", require("./authentication/routes"));
+//Route for authentication
+router.use("/zerozilla/auth", require("./authentication/routes"));
+
+//Route for agency
+router.use("/zerozilla/agency", require("./agency/routes"));
 
 module.exports = router;
