dd0553c2e0223bfec6c867a853dbeb2c5cc0e36f
premrajzambre <premrajzambre@gmail.com>
2022-09-13 22:02:13 +0530

    Authentication completed

--- routes/authentication/login/routes.js
+++ routes/authentication/login/routes.js
@@ -1,7 +1,24 @@
 require("../../../db/config");
 const express = require("express");
+const User = require("../../../db/schema/user");
 const router = express.Router();
 
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  try {
+    const { email, password } = await req.body;
+    if (email && password) {
+      let user = await User.findOne({ email, password }).select("-password");
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(204).json({ message: "No user found" });
+      }
+    } else {
+      res.status(202).json({ message: "email or password missing" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
--- routes/authentication/registration/routes.js
+++ routes/authentication/registration/routes.js
@@ -4,10 +4,13 @@ const router = express.Router();
 const User = require("../../../db/schema/user");
 
 router.post("/", async (req, res) => {
+  console.log("Request body :", req.body);
   try {
     let user = new User(req.body);
     let result = await user.save();
-    res.status(200).json(result);
+    result = result.toObject();
+    delete result.password;
+    res.status(201).json(result);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
